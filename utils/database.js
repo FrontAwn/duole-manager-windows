298e251902995cd56b0a2af613aec6ece8f30018
maple <1705757043@qq.com>
2019-02-22 18:55:32 +0800

    update database model ...

--- utils/database.js
+++ utils/database.js
@@ -1,30 +1,20 @@
 const sequelize = require("sequelize")
-const configs = require("../config/mysql.config.js")
+const mysqlConfig = require("../config/mysql.config.js")
+const env = mysqlConfig["env"]
+exports.getDuappResource = ()=>{
+	return new sequelize(
+		mysqlConfig['DuappResource'][env]['database'],
+		mysqlConfig['DuappResource'][env]['username'],
+		mysqlConfig['DuappResource'][env]['password'],
+		mysqlConfig['DuappResource'][env]['extra'],
+	)
+}
 
-exports.DuappResourceRemote = new sequelize(
-	configs['DuappResource']['remote']['database'],
-	configs['DuappResource']['remote']['username'],
-	configs['DuappResource']['remote']['password'],
-	configs['DuappResource']['remote']['extra'],
-)
-
-exports.DuappResourceLocal = new sequelize(
-	configs['DuappResource']['local']['database'],
-	configs['DuappResource']['local']['username'],
-	configs['DuappResource']['local']['password'],
-	configs['DuappResource']['local']['extra'],
-)
-
-exports.SjResourceRemote = new sequelize(
-	configs['SjResource']['remote']['database'],
-	configs['SjResource']['remote']['username'],
-	configs['SjResource']['remote']['password'],
-	configs['SjResource']['remote']['extra'],
-)
-
-exports.SjResourceLocal = new sequelize(
-	configs['SjResource']['local']['database'],
-	configs['SjResource']['local']['username'],
-	configs['SjResource']['local']['password'],
-	configs['SjResource']['local']['extra'],
-)
+exports.getSjResource = ()=>{
+	return new sequelize(
+		mysqlConfig['SjResource'][env]['database'],
+		mysqlConfig['SjResource'][env]['username'],
+		mysqlConfig['SjResource'][env]['password'],
+		mysqlConfig['SjResource'][env]['extra'],
+	)
+}
--- utils/model.js
+++ utils/model.js
@@ -1,74 +1,45 @@
 const databases = require("./database.js")
 const modelConfig = require("../config/model.config.js")
-const DuappResourceLocal = databases['DuappResourceLocal']
-const DuappResourceRemote = databases['DuappResourceRemote']
-const SjResourceLocal = databases['SjResourceLocal']
-const SjResourceRemote = databases['SjResourceRemote']
+const DuappResource = databases.getDuappResource()
+const SjResource = databases.getSjResource()
 
-const NikeProductList = {
-	"local":DuappResourceLocal.define(
-		modelConfig['NikeProductList']['tableName'],
-		modelConfig['NikeProductList']['structure'],
-		modelConfig['NikeProductList']['extra'],
-	),
-	"remote":DuappResourceRemote.define(
-		modelConfig['NikeProductList']['tableName'],
-		modelConfig['NikeProductList']['structure'],
-		modelConfig['NikeProductList']['extra'],
-	),
+const NikeProductList = DuappResource.define(
+	modelConfig['NikeProductList']['tableName'],
+	modelConfig['NikeProductList']['structure'],
+	modelConfig['NikeProductList']['extra'],
+)
+
+const SelfProductList = DuappResource.define(
+	modelConfig['SelfProductList']['tableName'],
+	modelConfig['SelfProductList']['structure'],
+	modelConfig['SelfProductList']['extra'],
+)
+
+const DuSkuList = SjResource.define(
+	modelConfig['DuSkuList']['tableName'],
+	modelConfig['DuSkuList']['structure'],
+	modelConfig['DuSkuList']['extra'],
+)
+
+const DuSkuDetail = SjResource.define(
+	modelConfig['DuSkuDetail']['tableName'],
+	modelConfig['DuSkuDetail']['structure'],
+	modelConfig['DuSkuDetail']['extra'],
+)
+
+exports.getNikeProductList = ()=>{
+	return NikeProductList
 }
 
-const SelfProductList = {
-	"local":DuappResourceLocal.define(
-		modelConfig['SelfProductList']['tableName'],
-		modelConfig['SelfProductList']['structure'],
-		modelConfig['SelfProductList']['extra'],
-	),
-	"remote":DuappResourceRemote.define(
-		modelConfig['SelfProductList']['tableName'],
-		modelConfig['SelfProductList']['structure'],
-		modelConfig['SelfProductList']['extra'],
-	),
+
+exports.getSelfProductList = ()=>{
+	return SelfProductList
 }
 
-const DuSkuList = {
-	"local":SjResourceLocal.define(
-		modelConfig['DuSkuList']['tableName'],
-		modelConfig['DuSkuList']['structure'],
-		modelConfig['DuSkuList']['extra'],
-	),
-	"remote":SjResourceRemote.define(
-		modelConfig['DuSkuList']['tableName'],
-		modelConfig['DuSkuList']['structure'],
-		modelConfig['DuSkuList']['extra'],
-	),
+exports.getDuSkuList = ()=>{
+	return DuSkuList
 }
 
-const DuSkuDetail = {
-	"local":SjResourceLocal.define(
-		modelConfig['DuSkuDetail']['tableName'],
-		modelConfig['DuSkuDetail']['structure'],
-		modelConfig['DuSkuDetail']['extra'],
-	),
-	"remote":SjResourceRemote.define(
-		modelConfig['DuSkuDetail']['tableName'],
-		modelConfig['DuSkuDetail']['structure'],
-		modelConfig['DuSkuDetail']['extra'],
-	),
-}
-
-exports.getNikeProductListModel = env=>{
-	return NikeProductList[env]
-}
-
-exports.getSelfProductListModel = env=>{
-	return SelfProductList[env]
-}
-
-exports.getDuSkuListModel = env=>{
-	return DuSkuList[env]
-}
-
-exports.getDuSkuDetailModel = env=>{
-	return DuSkuDetail[env]
+exports.getDuSkuDetail = ()=>{
+	return DuSkuDetail
 }
