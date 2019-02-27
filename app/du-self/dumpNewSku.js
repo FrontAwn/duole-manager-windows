4382723e65430d46f38ff072d57aa6b553f1fd95
maple <1705757043@qq.com>
2019-02-27 13:56:52 +0800

    uodate file name

--- app/du-self/dumpNewSku.js
+++
@@ -1,6 +0,0 @@
-const Common = require("../../utils/common.js")
-const Request = require("../../utils/request.js")
-
-const getNewProduct = async ()=>{
-	
-}
---
+++ app/du-self/newList.js
@@ -0,0 +1,8 @@
+const Common = require("../../utils/common.js")
+const Request = require("../../utils/request.js")
+
+const baseUrl = `https://m.poizon.com/search/list?size=[]&title=AQ1005-001%20%20&typeId=0&catId=0&unionId=0&sortType=0&sortMode=1&page=0&limit=20&sign=4eca3fc92f22f18b34d1630c42d04966`
+
+const getNewProductssKus = async ()=>{
+	
+}
