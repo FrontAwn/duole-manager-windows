a2660a53f767e8a3bc514b961ce181d60115b4cc
chengyufeng <541318591@qq.com>
2019-02-27 15:57:47 +0800

    write new list

--- app/du-self/newList.js
+++ app/du-self/newList.js
@@ -1,8 +1,43 @@
+const path = require("path")
 const Common = require("../../utils/common.js")
 const Request = require("../../utils/request.js")
 
-const baseUrl = `https://m.poizon.com/search/list?size=[]&title=AQ1005-001%20%20&typeId=0&catId=0&unionId=0&sortType=0&sortMode=1&page=0&limit=20&sign=4eca3fc92f22f18b34d1630c42d04966`
+const sign = "4eca3fc92f22f18b34d1630c42d04966"
 
-const getNewProductssKus = async ()=>{
-	
+const baseUrl = `https://m.poizon.com/search/list?size=[]&title=AQ1005-001%20%20&typeId=0&catId=0&unionId=0&sortType=0&sortMode=1&page=0&limit=20&sign=${sign}`
+
+const newList = []
+
+const getRequestUrl = sku=>{
+
 }
+
+const getNewProductSkus = async ()=>{
+	let where = {
+		type:0
+	}
+	let attrs = ['sku']
+	let res = await Request({
+		url:"/du/self/getProductList",
+		data:{
+			where:JSON.stringify(where),
+			attrs:JSON.stringify(attrs)
+		}
+	})
+	return res;
+}
+
+const getNewSkuRequestList = async url=>{
+
+}
+
+const writeNewSkuList = async ()=>{
+
+}
+
+;(async ()=>{
+
+	const newProductSkus = await getNewProductSkus()
+	console.log(newProductSkus)
+
+})()
