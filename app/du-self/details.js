e6ad366ac8cfd77c7f132127f2d999a47d3c4363
chengyufeng <541318591@qq.com>
2019-03-07 23:10:36 +0800

    modify capture detail

--- app/du-self/details.js
+++ app/du-self/details.js
@@ -2,59 +2,83 @@ const path = require("path")
 const moment = require("moment")
 const common = require("../../utils/common.js")
 const request = require("../../utils/request.js")
-const captureProductDetailParser = require("../../libs/du/captureProductDetailParser.js")
 
-captureProductDetailParser({
+const CaptureDetail = require("../../libs/du/captureDetail.js")
+const CaptureUtils = require("../../libs/du/utils.js")
 
-	captureBefore:async ()=>{
-		let conditions = {
-			where:JSON.stringify({
-				type:0
-			})
-		}
-		let res = await request({
-			url:"/du/self/getProductList",
-			data:conditions
+const getNeedCaptureProducts = async ()=>{
+	let conditions = {
+		where:JSON.stringify({
+			type:2,
+			url:{
+				"$ne":""
+			}
 		})
+	}
+	let res = await request({
+		url:"/du/self/getProductList",
+		data:conditions
+	})
 
-		let datas = res["data"]
-		let length = datas.length
+	return res["data"]
+}
 
-		if ( length > 0 ) {
-			console.log(`[Notice]: 当前存在个${length}新货号需要处理`)	
-			process.exit()
+const getRestProducts = async (needCaptureProducts,alreadyCaptureProductIds)=>{
+	let res = []
+	if ( alreadyCaptureProductIds.length === 0 ) {
+		res = common.deepCopy(needCaptureProducts)
+	} else {
+		for ( let [idx,content] of needCaptureProducts.entries() ) {
+			if ( !alreadyCaptureProductIds.includes(content["product_id"]) ) {
+				res.push(content)
+			}
 		}
-	},
+	}
+	return res;
+}
 
-	getProducts:async ()=>{
-		let conditions = {
-			where:JSON.stringify({
-				type:2,
-				url:{
-					"$ne":""
+
+;(async ()=>{
+
+	await CaptureDetail({
+		captureBefore: async ()=>{
+			let conditions = {
+				where:JSON.stringify({
+					type:0
+				})
+			}
+			let res = await request({
+				url:"/du/self/getProductList",
+				data:conditions
+			})
+
+			let datas = res["data"]
+			let length = datas.length
+
+			if ( length > 0 ) {
+				console.log(`[Notice]: 当前存在个${length}新货号需要处理`)	
+				process.exit()
+			}
+		},
+
+		setCaptureProducts: async ()=>{
+			let needCaptureProducts = await getNeedCaptureProducts();
+			let alreadyCaptureProductIds = await CaptureUtils.getAlreadyCaptureProductId("detail")
+			let products = await getRestProducts(needCaptureProducts,alreadyCaptureProductIds)
+			return products;
+		},
+
+		getCaptureDetail: async detail=>{
+			await request({
+				url:"/du/self/putConfimSkuDetail",
+				data:{
+					detail:JSON.stringify(detail),
 				}
 			})
 		}
-		let res = await request({
-			url:"/du/self/getProductList",
-			data:conditions
-		})
-		return res["data"]
-	},
+	})
 
-	setDetail:async detail=>{
-		await request({
-			url:"/du/self/putConfimSkuDetail",
-			data:{
-				detail:JSON.stringify(detail),
-			}
-		})
-	},
-
-	getAlreadyCapture:"/du/self/getAlreadyCaptureByDetail",
-	setAlreadyCapture:"/du/self/setAlreadyCaptureByDetail",
-	cleanAlreadyCapture:"/du/self/cleanAlreadyCaptureByDetail"
-})
+})()
 
 
 
---
+++ libs/du/captureDetail.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const moment = require("moment")
+const request = require("../../utils/request.js")
+const response = require("../../utils/response.js")
+const common = require("../../utils/common.js")
+const utils = require("./utils.js")
+
+module.exports = async options=>{
+
+	let captureBefore = options.captureBefore || null
+
+	let setCaptureProducts = options.setCaptureProducts || null
+
+	let getCaptureDetail = options.getCaptureDetail || null
+
+	if ( captureBefore !== null && typeof captureBefore === "function" ) {
+		await captureBefore()
+	}
+
+	if ( setCaptureProducts === null || typeof setCaptureProducts !== "function" ) {
+		console.log(`[Error]: 请传入setCaptureProducts函数，并返回包括要抓取的数据集`)
+		process.exit()
+	}
+
+	if ( getCaptureDetail === null || typeof getCaptureDetail !== "function" ) {
+		console.log(`[Error]: 请传入getCaptureDetail函数，并接受抓取后的detail数据作为参数`)
+	}
+
+	let needCaptureProducts = await setCaptureProducts()
+
+	if ( needCaptureProducts.length !== 0 ) {
+		console.log(`[Notice]: 当前需要抓取${needCaptureProducts.length}个货号`)
+		let currentCaptureIdx = 0 
+		for ( let [idx,content] of needCaptureProducts.entries() ) {
+			let url = content["url"]
+			let productId = content["product_id"]
+			let res = await common.httpGet(url)
+			let detail = response.parseProductDetail(res)
+			await getCaptureDetail(detail)
+			currentCaptureIdx += 1
+			await utils.setAlreadyCaptureProductId("detail",productId)
+			console.log(`----------> 已经完成[ ${currentCaptureIdx}/${needCaptureProducts.length} ] `)
+			await common.awaitTime(300)
+		}
+	} else {
+		console.log("[Notice]: 当天数据已经抓取完成一轮，正在消除缓存重新抓取")
+		await utils.cleanAlreadyCaptureProductId("detail")
+		console.log("[Notice]: 缓存已经清除完成，可以重新抓取")
+		process.exit()
+	}
+
+}
